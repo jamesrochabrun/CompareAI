3dd9855a30e777af5a323e21ec5fb1da728e5c38
jamesrochabrun <jamesrochabrun@gmail.com>
2024-06-20 23:59:18 -0700

    Initial Commit

---
+++ CompareAI/CompareAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompareAIApp.swift
+//  CompareAI
+//
+//  Created by James Rochabrun on 6/20/24.
+//
+
+import SwiftUI
+
+@main
+struct CompareAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
